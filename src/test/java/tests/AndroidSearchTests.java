544a3603ef04bb6cea886bf10e61aa71c1a344fc
Dmitriy Lyubtsov <dlyubtsov@rvision.ru>
2022-10-29 22:57:32 +0500

    do my lesson 23 homework

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -1,9 +1,11 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
@@ -19,5 +21,20 @@ public class AndroidSearchTests extends TestBase{
         step("Verify content found", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    void searchAndFollowingTest() {
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("test automation");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
+        step("Following first link", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).first().click());
+        step("Verify content contains error", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/view_wiki_error_text"))).shouldHave(text("An error occurred"));
     }
 }
