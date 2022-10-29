a170a89ff46c9ac78ddf5606e81d7050800fc75f
Dmitriy Lyubtsov <dlyubtsov@rvision.ru>
2022-10-29 22:13:22 +0500

    do my lesson 23 homework

---
+++ src/test/java/drivers/Config.java
@@ -0,0 +1,33 @@
+package drivers;
+
+@org.aeonbits.owner.Config.Sources({
+        "classpath:remote.properties"
+})
+public interface Config extends org.aeonbits.owner.Config {
+    @Key("browserstack.user")
+    String getBrowserstackUser();
+
+    @Key("browserstack.key")
+    String getBrowserstackKey();
+
+    @Key("appUrl")
+    String getAppUrl();
+
+    @Key("device")
+    String getDevice();
+
+    @Key("os_version")
+    String getOsVersion();
+
+    @Key("project")
+    String getProject();
+
+    @Key("build")
+    String getBuild();
+
+    @Key("name")
+    String getName();
+
+    @Key("remoteUrl")
+    String getRemoteUrl();
+}
